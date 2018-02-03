32fb4838b8ca94cae15180c05f4e7ad6a6bea980
Wei Wang <wvw@google.com>
2018-02-02 21:32:05 -0800

    sm8450-common: Remove QTI perfd
    Bug: 69270928
    Test: Build and boot device and test camera
    Change-Id: I87b283206f462fb5c8ec9cdd303ae2934fe9bfc4

--- common.mk
+++ common.mk
@@ -435,10 +435,6 @@ PRODUCT_PACKAGES += \
     libsqlite.vendor:64 \
     libvndfwk_detect_jni.qti.vendor
 
-# QTI service tracker
-PRODUCT_PACKAGES += \
-    vendor.qti.hardware.servicetracker@1.2.vendor
-
 # Remove unwanted packages
 PRODUCT_PACKAGES += \
     RemovePackages
