5cb021257c332f52696117c71e1d528ec207283a
Jake Weinstein <jake@aospa.co>
2023-02-09 01:42:04 +0900

    sm8450-common: Use common compatibility matrix
    Change-Id: I877824cc6c15cee7bdae4457984a6130430efd11

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -78,7 +78,6 @@ TARGET_SURFACEFLINGER_UDFPS_LIB := //hardware/oplus:libudfps_extension.oplus
 # HIDL
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := $(COMMON_PATH)/configs/vintf/device_framework_matrix.xml
 DEVICE_FRAMEWORK_MANIFEST_FILE += $(COMMON_PATH)/configs/vintf/framework_manifest.xml
-DEVICE_MATRIX_FILE := device/qcom/vendor-common/compatibility_matrix.xml
 DEVICE_MANIFEST_FILE := $(COMMON_PATH)/configs/vintf/manifest.xml
 ODM_MANIFEST_FILES := $(COMMON_PATH)/configs/vintf/manifest_odm.xml
 
