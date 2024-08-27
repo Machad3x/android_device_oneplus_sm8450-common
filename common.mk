7a54c598a983f9941adf36ef80b09c4ea202c2c9
pppig236 <priv@pppig236.com>
2024-08-27 10:56:08 -0400

    sm8450-common: Add vendor.oplus.hardware.osense.client-V1-ndk_platform for legacy blobs
    Signed-off-by: pppig236 <priv@pppig236.com>

--- common.mk
+++ common.mk
@@ -391,6 +391,7 @@ PRODUCT_PACKAGES += \
 
 # OSENSE
 PRODUCT_PACKAGES += \
+    vendor.oplus.hardware.osense.client-V1-ndk_platform.vendor \
     vendor.oplus.hardware.osense.client-service
 
 # Overlays
